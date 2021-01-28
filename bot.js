4a208e7288566abc232065cb43d7e3c39b931021
mbconnected97 <78173877+mbconnected97@users.noreply.github.com>
2021-01-28 14:24:30 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.ODA0MjQxMjE5NzQzMjUyNDgw.YBJeAA.C2uXGWBkP-RHL_ToqY3BY-YwfT4);//BOT_TOKEN is the Client Secret
